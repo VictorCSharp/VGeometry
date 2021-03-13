8ba693a69ffe7ce8b507231371f68ad2c0d7d6b3
Ivan <ivan.pavlov@adastragrp.com>
2021-03-13 12:50:49 +0200

    added method for segment intersection and test cases

--- VGeometry/Segment.cs
+++ VGeometry/Segment.cs
@@ -6,19 +6,128 @@ using System.Threading.Tasks;
 
 namespace VGeometry
 {
-    class Segment
+    public class Segment
     {
-        Point K;
-        Point L;
-        public Segment (Point K, Point L)
+        public Point P1;
+        public Point P2;
+        public Segment(Point P1, Point P2)
         {
-            this.K = K;
-            this.L = L;
-
+            this.P1 = P1;
+            this.P2 = P2;
         }
+
        public double Length ()
+       {
+            return Point.distance(P1, P2);
+       }
+        /// <summary>
+        /// Check if the Segment conatins the Point
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public bool Contains(Point p)
         {
-          return  Point.distance(K, L);
+            double x = p.x, y = p.y;
+
+            return 
+                (
+                    (x >= P1.x && x <= P2.x) || (x >= P2.x && x <= P1.x)
+                )
+            &&
+                (   
+                    (y >= P1.y && y <= P2.y) || (y >= P2.y && y <= P1.y)
+                );
+        }
+
+        /// <summary>
+        /// Checks if two lines intersect and returns the Point of intersection.
+        /// If parallel retur Infinity
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="tolerance">Difference tollerance</param>
+        /// <returns>Point</returns>
+       public Point Intersect(Segment s, double tolerance = 0.000001)
+       {
+            double x1 = P1.x, y1 = P1.y;
+            double x2 = P2.x, y2 = P2.y;
+
+            double x3 = s.P1.x, y3 = s.P1.y;
+            double x4 = s.P2.x, y4 = s.P2.y;
+
+            // equations of the form x = c (two vertical lines)
+            if (Math.Abs(x1 - x2) < tolerance && Math.Abs(x3 - x4) < tolerance && Math.Abs(x1 - x3) < tolerance)
+            {
+                throw new Exception("Both lines overlap vertically, ambiguous intersection points.");
+            }
+
+            //equations of the form y=c (two horizontal lines)
+            if (Math.Abs(y1 - y2) < tolerance && Math.Abs(y3 - y4) < tolerance && Math.Abs(y1 - y3) < tolerance)
+            {
+                throw new Exception("Both lines overlap horizontally, ambiguous intersection points.");
+            }
+
+            //equations of the form x=c (two vertical lines)
+            if (Math.Abs(x1 - x2) < tolerance && Math.Abs(x3 - x4) < tolerance)
+            {
+                return Point.Infinity;
+            }
+
+            //equations of the form y=c (two horizontal lines)
+            if (Math.Abs(y1 - y2) < tolerance && Math.Abs(y3 - y4) < tolerance)
+            {
+                return Point.Infinity;
+            }
+
+            double x, y;
+
+            //lineA is vertical x1 = x2 - slope will be infinity
+            if (Math.Abs(x1 - x2) < tolerance)
+            {
+                //compute slope of line 2 (m2) and c2
+                double m2 = (y4 - y3) / (x4 - x3);
+                double c2 = -m2 * x3 + y3;
+
+                //equation of vertical line is x = c
+                x = x1;
+                y = c2 + m2 * x1;
+            }
+            //lineB is vertical x3 = x4 - slope will be infinity
+            else if (Math.Abs(x3 - x4) < tolerance)
+            {
+                //compute slope of line 1 (m1) and c2
+                double m1 = (y2 - y1) / (x2 - x1);
+                double c1 = -m1 * x1 + y1;
+
+                x = x3;
+                y = c1 + m1 * x3;
+            }
+            //lineA & lineB are not vertical 
+            //(could be horizontal we can handle it with slope = 0)
+            else
+            {
+                //compute slope of line 1 (m1) and c2
+                double m1 = (y2 - y1) / (x2 - x1);
+                double c1 = -m1 * x1 + y1;
+
+                //compute slope of line 2 (m2) and c2
+                double m2 = (y4 - y3) / (x4 - x3);
+                double c2 = -m2 * x3 + y3;
+
+                //solving equations
+                x = (c1 - c2) / (m2 - m1);
+                y = c2 + m2 * x;
+
+                //verify by plugging intersection point (x, y)
+                //in orginal equations to see if they intersect
+                //otherwise x,y values will not be finite and will fail this check
+                if (!(Math.Abs(-m1 * x + y - c1) < tolerance
+                    && Math.Abs(-m2 * x + y - c2) < tolerance))
+                {
+                    return new Point(Double.NaN, Double.NaN);
+                }
+            }
+
+            return new Point(x, y);
         }
             
     }
---
+++ VGeometryTests/SegmentTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VGeometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VGeometry.Tests
+{
+    [TestClass()]
+    public class SegmentTests
+    {
+
+        [TestMethod()]
+        public void LengthTest()
+        {
+            Point P = new Point(0, 0);
+            Point Q = new Point(1, 1);
+            Segment s = new Segment(P, Q);
+
+            Assert.IsTrue(s.Length().Equals(Math.Sqrt(2)));
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+            Point P = new Point(0, 0);
+            Point Q = new Point(1, 1);
+            Segment s = new Segment(P, Q);
+            Point M = new Point(0.5, 0.5);
+
+            Assert.IsTrue(s.Contains(M));
+        }
+
+        [TestMethod()]
+        public void NotContainsTest()
+        {
+            Point P = new Point(0, 0);
+            Point Q = new Point(1, 1);
+            Segment s = new Segment(P, Q);
+            Point M = new Point(1.5, 0.5);
+
+            Assert.IsFalse(s.Contains(M));
+        }
+
+        [TestMethod()]
+        public void IntersectTest()
+        {
+            Point P1 = new Point(0, 0);
+            Point Q1 = new Point(1, 1);
+            Segment s1 = new Segment(P1, Q1);
+
+            Point P2 = new Point(0, 1);
+            Point Q2 = new Point(1, 0);
+            Segment s2 = new Segment(P2, Q2);
+
+            Point X = s1.Intersect(s2);
+
+            Assert.IsTrue(X.x.Equals(0.5)&&X.y.Equals(0.5));
+        }
+    }
+}
